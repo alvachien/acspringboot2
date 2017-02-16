70b4f025735235a06a1efbbbc8abcfe51d028e46
Alva Chien <alvachien@live.com>
2017-02-16 21:07:56 +0800

    Initial commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.alvachien.learning.java.acspringboot2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
